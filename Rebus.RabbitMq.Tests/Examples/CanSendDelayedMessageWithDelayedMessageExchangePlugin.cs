39f83475782b430fe9179aab9f8a9e4c5f2375ad
René Schultz Madsen <rm@microting.com>
2023-04-06 07:23:46 +0000

    Disabling the RabbitMQ Delayed Message Plugin from the code, since we don't need it for moving forward. This will be reinstated when test platform is ready to test for it.

--- Rebus.RabbitMq.Tests/Examples/CanSendDelayedMessageWithDelayedMessageExchangePlugin.cs
+++ Rebus.RabbitMq.Tests/Examples/CanSendDelayedMessageWithDelayedMessageExchangePlugin.cs
@@ -19,85 +19,86 @@ namespace Rebus.RabbitMq.Tests.Examples;
 [TestFixture]
 public class CanSendDelayedMessageWithDelayedMessageExchangePlugin : FixtureBase
 {
-    [Test]
-    public async Task ShowHowItIsDone_TimeoutManager()
-    {
-        DeclareDelayedMessageExchange("RebusDelayed");
-
-        using var gotTheMessage = new ManualResetEvent(initialState: false);
-
-        var stopwatch = new Stopwatch();
-
-        using var receiver = new BuiltinHandlerActivator();
-
-        receiver.Handle<string>(async _ =>
-        {
-            stopwatch.Stop();
-            gotTheMessage.Set();
-        });
-
-        Configure.With(receiver)
-            .Transport(t => t.UseRabbitMq(RabbitMqTransportFactory.ConnectionString, "receiver"))
-            .Routing(r => r.TypeBased().Map<string>("receiver"))
-            .Timeouts(t => t.UseDelayedMessageExchange("RebusDelayed"))
-            .Start();
-
-
-        stopwatch.Start();
-        await receiver.Bus.Defer(TimeSpan.FromSeconds(5), "HEJ MED DIG");
-
-        gotTheMessage.WaitOrDie(timeout: TimeSpan.FromSeconds(10));
-
-        var elapsed = stopwatch.Elapsed;
-
-        Assert.That(elapsed, Is.GreaterThan(TimeSpan.FromSeconds(5)));
-    }
-
-    [Test]
-    public async Task ShowHowItIsDone_Manual()
-    {
-        DeclareDelayedMessageExchange("RebusDelayed");
-
-        using var gotTheMessage = new ManualResetEvent(initialState: false);
-
-        var stopwatch = new Stopwatch();
-
-        using var receiver = new BuiltinHandlerActivator();
-
-        receiver.Handle<string>(async _ =>
-        {
-            stopwatch.Stop();
-            gotTheMessage.Set();
-        });
-
-        Configure.With(receiver)
-            .Transport(t => t.UseRabbitMq(RabbitMqTransportFactory.ConnectionString, "receiver"))
-            .Routing(r => r.TypeBased().Map<string>("receiver@RebusDelayed"))
-            .Start();
-
-
-        stopwatch.Start();
-        await receiver.Bus.Send("HEJ MED DIG", new Dictionary<string, string> { ["x-delay"] = "5000" });
-
-        gotTheMessage.WaitOrDie(timeout: TimeSpan.FromSeconds(10));
-
-        var elapsed = stopwatch.Elapsed;
-
-        Assert.That(elapsed, Is.GreaterThan(TimeSpan.FromSeconds(5)));
-    }
-
-    static void DeclareDelayedMessageExchange(string exchangeName)
-    {
-        var connectionFactory = new ConnectionFactory { Uri = new(RabbitMqTransportFactory.ConnectionString) };
-        using var connection = connectionFactory.CreateConnection();
-        using var model = connection.CreateModel();
-
-        model.ExchangeDeclare(
-            exchange: exchangeName,
-            type: "x-delayed-message",
-            durable: true,
-            autoDelete: false,
-            arguments: new Dictionary<string, object> { ["x-delayed-type"] = "direct" }
-        );
-    }
+    // TODO uncomment when test platform is set correctly for enabling delayed message exchange plugin
+    // [Test]
+    // public async Task ShowHowItIsDone_TimeoutManager()
+    // {
+    //     DeclareDelayedMessageExchange("RebusDelayed");
+    //
+    //     using var gotTheMessage = new ManualResetEvent(initialState: false);
+    //
+    //     var stopwatch = new Stopwatch();
+    //
+    //     using var receiver = new BuiltinHandlerActivator();
+    //
+    //     receiver.Handle<string>(async _ =>
+    //     {
+    //         stopwatch.Stop();
+    //         gotTheMessage.Set();
+    //     });
+    //
+    //     Configure.With(receiver)
+    //         .Transport(t => t.UseRabbitMq(RabbitMqTransportFactory.ConnectionString, "receiver"))
+    //         .Routing(r => r.TypeBased().Map<string>("receiver"))
+    //         .Timeouts(t => t.UseDelayedMessageExchange("RebusDelayed"))
+    //         .Start();
+    //
+    //
+    //     stopwatch.Start();
+    //     await receiver.Bus.Defer(TimeSpan.FromSeconds(5), "HEJ MED DIG");
+    //
+    //     gotTheMessage.WaitOrDie(timeout: TimeSpan.FromSeconds(10));
+    //
+    //     var elapsed = stopwatch.Elapsed;
+    //
+    //     Assert.That(elapsed, Is.GreaterThan(TimeSpan.FromSeconds(5)));
+    // }
+    //
+    // [Test]
+    // public async Task ShowHowItIsDone_Manual()
+    // {
+    //     DeclareDelayedMessageExchange("RebusDelayed");
+    //
+    //     using var gotTheMessage = new ManualResetEvent(initialState: false);
+    //
+    //     var stopwatch = new Stopwatch();
+    //
+    //     using var receiver = new BuiltinHandlerActivator();
+    //
+    //     receiver.Handle<string>(async _ =>
+    //     {
+    //         stopwatch.Stop();
+    //         gotTheMessage.Set();
+    //     });
+    //
+    //     Configure.With(receiver)
+    //         .Transport(t => t.UseRabbitMq(RabbitMqTransportFactory.ConnectionString, "receiver"))
+    //         .Routing(r => r.TypeBased().Map<string>("receiver@RebusDelayed"))
+    //         .Start();
+    //
+    //
+    //     stopwatch.Start();
+    //     await receiver.Bus.Send("HEJ MED DIG", new Dictionary<string, string> { ["x-delay"] = "5000" });
+    //
+    //     gotTheMessage.WaitOrDie(timeout: TimeSpan.FromSeconds(10));
+    //
+    //     var elapsed = stopwatch.Elapsed;
+    //
+    //     Assert.That(elapsed, Is.GreaterThan(TimeSpan.FromSeconds(5)));
+    // }
+    //
+    // static void DeclareDelayedMessageExchange(string exchangeName)
+    // {
+    //     var connectionFactory = new ConnectionFactory { Uri = new(RabbitMqTransportFactory.ConnectionString) };
+    //     using var connection = connectionFactory.CreateConnection();
+    //     using var model = connection.CreateModel();
+    //
+    //     model.ExchangeDeclare(
+    //         exchange: exchangeName,
+    //         type: "x-delayed-message",
+    //         durable: true,
+    //         autoDelete: false,
+    //         arguments: new Dictionary<string, object> { ["x-delayed-type"] = "direct" }
+    //     );
+    // }
 }
--- Rebus.RabbitMq/Config/RabbitMqDelayedMessageExchangeExtensions.cs
+++ Rebus.RabbitMq/Config/RabbitMqDelayedMessageExchangeExtensions.cs
@@ -1,119 +1,120 @@
-﻿using System;
-using System.Globalization;
-using System.Threading;
-using System.Threading.Tasks;
-using Rebus.Bus;
-using Rebus.Exceptions;
-using Rebus.Extensions;
-using Rebus.Logging;
-using Rebus.Messages;
-using Rebus.RabbitMq;
-using Rebus.Timeouts;
-using Rebus.Transport;
-
-namespace Rebus.Config;
-
-/// <summary>
-/// Configuration extensions for enabling Rebus' use of RabbitMQ's Delayed Message Exchange plugin.
-/// See here: https://github.com/rabbitmq/rabbitmq-delayed-message-exchange
-/// </summary>
-public static class RabbitMqDelayedMessageExchangeExtensions
-{
-    /// <summary>
-    /// Configures Rebus to use a delayed message exchange with the name <paramref name="exchangeName"/>. The exchange will be automatically declared.
-    /// </summary>
-    public static void UseDelayedMessageExchange(this StandardConfigurer<ITimeoutManager> configurer, string exchangeName, bool automaticallyDeclareExchange = true)
-    {
-        if (configurer == null) throw new ArgumentNullException(nameof(configurer));
-        if (exchangeName == null) throw new ArgumentNullException(nameof(exchangeName));
-
-        configurer
-            .OtherService<Options>()
-            .Decorate(c =>
-            {
-                var options = c.Get<Options>();
-
-                var timeoutManagerAddress = $"@{exchangeName}";
-
-                if (!string.IsNullOrWhiteSpace(options.ExternalTimeoutManagerAddressOrNull))
-                {
-                    throw new RebusConfigurationException($"Cannot set '{timeoutManagerAddress}' as the timeout manager address, because it was already set to '{options.ExternalTimeoutManagerAddressOrNull}'");
-                }
-
-                options.ExternalTimeoutManagerAddressOrNull = timeoutManagerAddress;
-
-                return options;
-            });
-
-        configurer
-            .OtherService<ITransport>()
-            .Decorate(c => new DelayedMessageExchangeTransportDecorator(
-                rebusLoggerFactory: c.Get<IRebusLoggerFactory>(),
-                transport: c.Get<ITransport>(),
-                options: c.Get<Options>(),
-                rabbitMqTransport: c.Get<RabbitMqTransport>(),
-                declareExchange: automaticallyDeclareExchange
-            ));
-    }
-
-    class DelayedMessageExchangeTransportDecorator : ITransport, IInitializable
-    {
-        readonly RabbitMqTransport _rabbitMqTransport;
-        readonly ITransport _transport;
-        readonly string _exchangeName;
-        readonly bool _declareExchange;
-        readonly ILog _logger;
-
-        public DelayedMessageExchangeTransportDecorator(IRebusLoggerFactory rebusLoggerFactory, ITransport transport, Options options, RabbitMqTransport rabbitMqTransport, bool declareExchange)
-        {
-            if (rebusLoggerFactory == null) throw new ArgumentNullException(nameof(rebusLoggerFactory));
-            _logger = rebusLoggerFactory.GetLogger<DelayedMessageExchangeTransportDecorator>();
-            _transport = transport ?? throw new ArgumentNullException(nameof(transport));
-            _rabbitMqTransport = rabbitMqTransport ?? throw new ArgumentNullException(nameof(rabbitMqTransport));
-            _declareExchange = declareExchange;
-            _exchangeName = options.ExternalTimeoutManagerAddressOrNull;
-
-            if (!_exchangeName.StartsWith("@"))
-            {
-                throw new ArgumentException(
-                    $"Expected the delayed message exchange name to start with '@', e.g. like '@RebusDelayed' or something like that - the value passed to the transport decorator via Options.ExternalTimeoutManagerAddressOrNull was '{options.ExternalTimeoutManagerAddressOrNull}");
-            }
-        }
-
-        public void CreateQueue(string address) => _transport.CreateQueue(address);
-
-        public Task<TransportMessage> Receive(ITransactionContext context, CancellationToken cancellationToken) => _transport.Receive(context, cancellationToken);
-
-        public void Initialize()
-        {
-            if (!_declareExchange) return;
-
-            _logger.Info("Delaring delayed message exchange with name {exchangeName}", _exchangeName);
-
-            _rabbitMqTransport.DeclareDelayedMessageExchange(_exchangeName);
-        }
-
-        public Task Send(string destinationAddress, TransportMessage message, ITransactionContext context)
-        {
-            var headers = message.Headers;
-
-            if (headers.TryGetValue(Headers.DeferredRecipient, out var recipient) && headers.TryGetValue(Headers.DeferredUntil, out var deferredUntil))
-            {
-                headers.Remove(Headers.DeferredUntil);
-                headers.Remove(Headers.DeferredRecipient);
-
-                var receiveTime = deferredUntil.ToDateTimeOffset();
-                var delay = receiveTime - DateTimeOffset.Now;
-                var delayMs = (int)delay.TotalMilliseconds;
-
-                headers["x-delay"] = delayMs.ToString(CultureInfo.InvariantCulture);
-
-                return _transport.Send($"{recipient}{_exchangeName}", message, context);
-            }
-
-            return _transport.Send(destinationAddress, message, context);
-        }
-
-        public string Address => _transport.Address;
-    }
-}
+﻿// TODO uncomment when test platform is set correctly for enabling delayed message exchange plugin
+// using System;
+// using System.Globalization;
+// using System.Threading;
+// using System.Threading.Tasks;
+// using Rebus.Bus;
+// using Rebus.Exceptions;
+// using Rebus.Extensions;
+// using Rebus.Logging;
+// using Rebus.Messages;
+// using Rebus.RabbitMq;
+// using Rebus.Timeouts;
+// using Rebus.Transport;
+//
+// namespace Rebus.Config;
+//
+// /// <summary>
+// /// Configuration extensions for enabling Rebus' use of RabbitMQ's Delayed Message Exchange plugin.
+// /// See here: https://github.com/rabbitmq/rabbitmq-delayed-message-exchange
+// /// </summary>
+// public static class RabbitMqDelayedMessageExchangeExtensions
+// {
+//     /// <summary>
+//     /// Configures Rebus to use a delayed message exchange with the name <paramref name="exchangeName"/>. The exchange will be automatically declared.
+//     /// </summary>
+//     public static void UseDelayedMessageExchange(this StandardConfigurer<ITimeoutManager> configurer, string exchangeName, bool automaticallyDeclareExchange = true)
+//     {
+//         if (configurer == null) throw new ArgumentNullException(nameof(configurer));
+//         if (exchangeName == null) throw new ArgumentNullException(nameof(exchangeName));
+//
+//         configurer
+//             .OtherService<Options>()
+//             .Decorate(c =>
+//             {
+//                 var options = c.Get<Options>();
+//
+//                 var timeoutManagerAddress = $"@{exchangeName}";
+//
+//                 if (!string.IsNullOrWhiteSpace(options.ExternalTimeoutManagerAddressOrNull))
+//                 {
+//                     throw new RebusConfigurationException($"Cannot set '{timeoutManagerAddress}' as the timeout manager address, because it was already set to '{options.ExternalTimeoutManagerAddressOrNull}'");
+//                 }
+//
+//                 options.ExternalTimeoutManagerAddressOrNull = timeoutManagerAddress;
+//
+//                 return options;
+//             });
+//
+//         configurer
+//             .OtherService<ITransport>()
+//             .Decorate(c => new DelayedMessageExchangeTransportDecorator(
+//                 rebusLoggerFactory: c.Get<IRebusLoggerFactory>(),
+//                 transport: c.Get<ITransport>(),
+//                 options: c.Get<Options>(),
+//                 rabbitMqTransport: c.Get<RabbitMqTransport>(),
+//                 declareExchange: automaticallyDeclareExchange
+//             ));
+//     }
+//
+//     class DelayedMessageExchangeTransportDecorator : ITransport, IInitializable
+//     {
+//         readonly RabbitMqTransport _rabbitMqTransport;
+//         readonly ITransport _transport;
+//         readonly string _exchangeName;
+//         readonly bool _declareExchange;
+//         readonly ILog _logger;
+//
+//         public DelayedMessageExchangeTransportDecorator(IRebusLoggerFactory rebusLoggerFactory, ITransport transport, Options options, RabbitMqTransport rabbitMqTransport, bool declareExchange)
+//         {
+//             if (rebusLoggerFactory == null) throw new ArgumentNullException(nameof(rebusLoggerFactory));
+//             _logger = rebusLoggerFactory.GetLogger<DelayedMessageExchangeTransportDecorator>();
+//             _transport = transport ?? throw new ArgumentNullException(nameof(transport));
+//             _rabbitMqTransport = rabbitMqTransport ?? throw new ArgumentNullException(nameof(rabbitMqTransport));
+//             _declareExchange = declareExchange;
+//             _exchangeName = options.ExternalTimeoutManagerAddressOrNull;
+//
+//             if (!_exchangeName.StartsWith("@"))
+//             {
+//                 throw new ArgumentException(
+//                     $"Expected the delayed message exchange name to start with '@', e.g. like '@RebusDelayed' or something like that - the value passed to the transport decorator via Options.ExternalTimeoutManagerAddressOrNull was '{options.ExternalTimeoutManagerAddressOrNull}");
+//             }
+//         }
+//
+//         public void CreateQueue(string address) => _transport.CreateQueue(address);
+//
+//         public Task<TransportMessage> Receive(ITransactionContext context, CancellationToken cancellationToken) => _transport.Receive(context, cancellationToken);
+//
+//         public void Initialize()
+//         {
+//             if (!_declareExchange) return;
+//
+//             _logger.Info("Delaring delayed message exchange with name {exchangeName}", _exchangeName);
+//
+//             _rabbitMqTransport.DeclareDelayedMessageExchange(_exchangeName);
+//         }
+//
+//         public Task Send(string destinationAddress, TransportMessage message, ITransactionContext context)
+//         {
+//             var headers = message.Headers;
+//
+//             if (headers.TryGetValue(Headers.DeferredRecipient, out var recipient) && headers.TryGetValue(Headers.DeferredUntil, out var deferredUntil))
+//             {
+//                 headers.Remove(Headers.DeferredUntil);
+//                 headers.Remove(Headers.DeferredRecipient);
+//
+//                 var receiveTime = deferredUntil.ToDateTimeOffset();
+//                 var delay = receiveTime - DateTimeOffset.Now;
+//                 var delayMs = (int)delay.TotalMilliseconds;
+//
+//                 headers["x-delay"] = delayMs.ToString(CultureInfo.InvariantCulture);
+//
+//                 return _transport.Send($"{recipient}{_exchangeName}", message, context);
+//             }
+//
+//             return _transport.Send(destinationAddress, message, context);
+//         }
+//
+//         public string Address => _transport.Address;
+//     }
+// }
